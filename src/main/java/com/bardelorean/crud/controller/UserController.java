e72f7d2ee0fe0d20f83db57a3169ba9695796b28
Sergei Vanin <barsabantuy@gmail.com>
2020-11-18 15:21:40 +0300

    bootstrap style

--- src/main/java/com/bardelorean/crud/controller/UserController.java
+++ src/main/java/com/bardelorean/crud/controller/UserController.java
@@ -1,46 +1,43 @@
 package com.bardelorean.crud.controller;
 
+import com.bardelorean.crud.model.Role;
 import com.bardelorean.crud.model.User;
 import com.bardelorean.crud.repository.UserRepository;
+import com.bardelorean.crud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Controller
 public class UserController {
+	private final UserService userService;
+	private final PasswordEncoder passwordEncoder;
 
-	final UserRepository userRepository;
 
-	public UserController(UserRepository userRepository) {
-		this.userRepository = userRepository;
+	public UserController(UserService userService, PasswordEncoder passwordEncoder) {
+		this.userService = userService;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@GetMapping("/user/{id}")
 	@PreAuthorize("#id == authentication.principal.id or hasRole('ADMIN')")
 	public String getUser(@PathVariable("id") long id, Model model) {
-		User user = userRepository.findById(id).orElseThrow();
+		User user = userService.findById(id);
 		model.addAttribute("user", user);
 		return "user";
 	}
 
 	@GetMapping
 	public String index() {
-//		Role roleAdmin = new Role("ROLE_ADMIN");
-//		Role roleUser = new Role("ROLE_USER");
-//		Set<Role> roles1 = new HashSet<>();
-//		Set<Role> roles2 = new HashSet<>();
-//		roles1.add(roleUser);
-//		roles2.add(roleUser);
-//		roles2.add(roleAdmin);
-//		User user1 = new User("Mike", (byte) 23, "pass", roles1);
-//		User user2 = new User("Jack", (byte) 28, "pass", roles2);
-//		userRepository.save(user1);
-//		userRepository.save(user2);
-//		System.out.println("Добавлены Юзеры");
 		return "redirect:/login";
 	}
 
--- src/main/java/com/bardelorean/crud/security/SecurityConfig.java
+++ src/main/java/com/bardelorean/crud/security/SecurityConfig.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -32,29 +34,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.csrf()
 				.disable()
 				.authorizeRequests()
-				.antMatchers("/admin/**").access("hasRole('ADMIN')")
-				.antMatchers("/user/**").access("hasAnyRole('ADMIN', 'USER')")
-				.anyRequest().authenticated()
+					.antMatchers("/admin/**").access("hasRole('ADMIN')")
+					.antMatchers("/user/**").access("hasAnyRole('ADMIN', 'USER')")
+					.antMatchers("/resources/**").permitAll()
+					.antMatchers("/bootstrap/**").permitAll()
+					.antMatchers("/popper/**").permitAll()
+					.antMatchers("/jquery/**").permitAll()
+					.anyRequest().authenticated()
 				.and()
-				.formLogin()
-				.loginPage("/login")
-				.usernameParameter("j_username")
-				.passwordParameter("j_password")
-				.successHandler(loginSuccessHandler)
-				.permitAll()
+					.formLogin()
+					.loginPage("/login")
+					.permitAll()
+					.usernameParameter("j_username")
+					.passwordParameter("j_password")
+					.successHandler(loginSuccessHandler)
 				.and()
-				.logout()
-				.permitAll()
-				.logoutSuccessUrl("/");
+					.logout()
+					.permitAll()
+					.logoutSuccessUrl("/");
 	}
 
 	@Autowired
-	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) {
+		auth.authenticationProvider(authProvider());
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService);
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
 	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 }
